8072d5954a381bee5ab046d303179e55d8d541c7
“Jakob <jnunnendorf@icloud.com>
2024-03-02 11:13:23 +0400

    refactor(home): make responsive

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,19 +3,23 @@ import Image from "next/image";
 export default function Home() {
   return (
       <main className="grid min-h-screen place-content-center h-full w-full">
-          <figure className="w-full h-full absolute">
-              <Image src={"/bg_stars.png"} fill alt="Starry night sky" />
+          <figure className="w-screen h-screen absolute">
+              <Image
+                  src={"/bg_stars.png"}
+                  alt="Starry night sky background image"
+                  fill
+                  className="object-cover"
+              />
           </figure>
-          <div className="z-10 text-center font-extrabold">
+          <div className="flex flex-col items-center justify-center z-10 text-center font-extrabold px-2">
               <h1 className="text-5xl text-white">jqode_ software</h1>
-              <h2 className="text-5xl text-white mt-4 mb-8">
-                  Presenting your business <br /> to the world.
+              <h2 className="lg:text-5xl text-2xl text-white mt-4 mb-8">
+                  Presenting your company <br /> to the world.
               </h2>
-              <a
-                  href={"mailto:info@jqode.eu"}
-                  className="py-2 px-4 rounded-lg text-xl text-white border-2 border-white"
-              >
-                  Start now
+              <a href={"mailto:info@jqode.eu"} className=" w-fit">
+                  <div className="py-2 px-4 w-fit rounded-lg text-xl text-white border-2 border-white">
+                      Start now
+                  </div>
               </a>
           </div>
       </main>
