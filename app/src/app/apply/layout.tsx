c81c20b4f4d5656a5c6a31004e586b5eddf17f0e
“Jakob <jnunnendorf@icloud.com>
2024-03-10 16:03:51 +0400

    fix(apply): make responsive

--- app/src/app/apply/layout.tsx
+++ app/src/app/apply/layout.tsx
@@ -1,7 +1,9 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <section className=" h-screen w-full flex justify-center items-center">
-            <div className="rounded-3xl bg-white w-5/12 h-2/3 ">{children}</div>
+            <div className="rounded-3xl bg-white lg:w-5/12 h-2/3 ">
+                {children}
+            </div>
         </section>
     );
 }
